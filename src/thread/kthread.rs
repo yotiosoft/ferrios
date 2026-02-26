8168be0dc38bc24ac5e32f38e3a84fb273de1583
yotiosoft <ytani0323@gmail.com>
2026-02-27 06:55:13 +0900

    move uprocess.rs

--- src/thread/kthread.rs
+++ src/thread/kthread.rs
@@ -5,7 +5,7 @@ pub const NTHREAD: usize = 64;
 /// カーネルスレッド作成
 pub fn create_kernel_thread(entry: fn() -> !) {
     // スレッド ID を確保
-    let tid = next_tid().expect("Thread table is full");
+    let tid = super::next_tid().expect("Thread table is full");
 
     // スタックを作成
     let stack = unsafe {
@@ -23,15 +23,4 @@ pub fn create_kernel_thread(entry: fn() -> !) {
     table[tid].context.rsp = stack_top;
     table[tid].context.rip = entry as u64;
     table[tid].context.rflags = 0x200;  // IF (Interrupt Flag) を有効化
-}
-
-/// スレッド ID 決定
-pub fn next_tid() -> Option<usize> {
-    let table = THREAD_TABLE.lock();
-    for i in 0..NTHREAD-1 {
-        if table[i].state == ThreadState::Unused {
-            return Some(i);
-        }
-    }
-    None
 }
